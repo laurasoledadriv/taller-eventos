f16cfb110ee635cfd74dd18dc799074d8b87f104
Laura <laurasoledadrivgomez@hotmail.com>
2023-09-02 13:16:48 -0300

    Creación de un div y un alerta en el div

---
+++ eventos.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded",()=>{
+let undiv = document.getElementById('container');
+  undiv.addEventListener('click', () => {
+    alert('Hola! Soy el div');
+  });
+})
